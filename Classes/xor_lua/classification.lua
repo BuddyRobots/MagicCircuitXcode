bda2aad3ac7f45d3686aa6b61190f93f9f8fdc84
PhilipZeratul <p.f.zeratul@gmail.com>
2016-10-11 11:18:25 +0800

    modified predict & loadModel

--- Classes/xor_lua/classification.lua
+++ Classes/xor_lua/classification.lua
@@ -1,54 +1,34 @@
 require 'torch'
 require 'nn'
-require 'optim'
 
-----------------------------------------
--- load test data for debug use
--- remove this part when release
-function loadData()
+torch.setdefaulttensortype('torch.FloatTensor')
+model = ""
 
-	require 'data'
-	load_test_data()
+function loadModel(path)
 
+	model = torch.load(path)
+    print ("Loaded Model -- Success")
+	
 end
 
 ----------------------------------------
--- input : 
--- output: int class
-function predict(imgPath, modelPath)
+-- input : float array[3*28*28]
+-- output: int   class
+function predictClass(arr)
 
-	local image = image.load(imgPath, 3, 'double')
-
-	local model = torch.load(modelPath)
+	local image = torch.Tensor(arr)
+	image:resize(3, 28, 28)
+	print ("Loaded Array -- Success")
 
 	-- pedict
-	local pred = model:forward(img)
+	local pred = model:forward(image)
 	local m_t, m_i = torch.max(pred, 1)
 
 	return m_i[1]
 
 end
 
-
 ----------------------------------------
---
---[[loadData()
-
-testSize = table.getn(test_data)
-errNum = 0
-
-for i = 1, testSize do
-	pred = predict(test_data[i][1])
-	print("class: " .. test_data[i][2] .. " <-> " .. pred)
-
-	if pred ~= test_data[i][2] then
-		errNum = errNum + 1
-	end
-end
-
-print("-ErrorNum: " .. errNum)
-print("-TestSize: " .. testSize)
-print("-Accuracy: " .. (testSize - errNum) / testSize)]]
 
 function test()
 	print("hello world!")
